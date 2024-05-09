5d5785548695d98b0768a6e651dcbeb3b82cd14f
tuioioi456 <sammyqweennam1311@outlook.com>
2024-05-09 20:39:41 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-function zigzagLevelOrder(root) {
-  if (!root) return [];
-  const result = [];
-  const queue = [root];
-  let zigzag = false;
-  while (queue.length) {
-    const levelSize = queue.length;
-    const currentLevel = [];
-    for (let i = 0; i < levelSize; i++) {
-      const node = queue.shift();
-      if (zigzag) currentLevel.unshift(node.val);
-      else currentLevel.push(node.val);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
+function moveZeroes(nums) {
+  let index = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[index++] = nums[i];
     }
-    result.push(currentLevel);
-    zigzag = !zigzag;
   }
-  return result;
+  for (let i = index; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
 }
