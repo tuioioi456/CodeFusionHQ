2a9e07c5360c05b45e58f30a18d1b2e98d90c165
tuioioi456 <sammyqweennam1311@outlook.com>
2024-05-10 17:37:47 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-function moveZeroes(nums) {
-  let index = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[index++] = nums[i];
-    }
-  }
-  for (let i = index; i < nums.length; i++) {
-    nums[i] = 0;
-  }
-  return nums;
+function averageArray(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
 }
