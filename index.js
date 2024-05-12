f0baa3f89da0796133de8d0ef1f706fc51490ef2
tuioioi456 <sammyqweennam1311@outlook.com>
2024-05-12 13:16:52 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) return i;
+function isValidBST(root) {
+  return isValid(root, null, null);
+  function isValid(node, min, max) {
+    if (!node) return true;
+    if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+      return false;
+    return (
+      isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+    );
   }
-  return -1;
 }
